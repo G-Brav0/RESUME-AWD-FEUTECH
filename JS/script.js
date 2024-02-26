3000c994015d44cbfa2c59db7bc1164815c2144b
G-Brav0 <138981966+G-Brav0@users.noreply.github.com>
2024-02-27 00:01:15 +0800

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+var cbtn = document.getElementById("color_btn");
+cbtn.addEventListener("click", changeColors);
+
+function changeColors() {
+    var pageBody = document.querySelector(".page-body");
+    // var refColor = document.querySelector("a");
+    // pageBody.style.backgroundColor = pageBody.style.backgroundColor === "grey" ? "#019938" : "grey";
+    // pageBody.style.backgroundColor = pageBody.style.backgroundColor === "teal" ? "grey" : "teal";
+    // pageBody.style.backgroundColor = pageBody.style.backgroundColor === "teal" ? "pastel" : "teal";
+    pageBody.style.backgroundColor = pageBody.style.backgroundColor === "teal" ? "turquoise" : "teal";
+    
+    // pageBody.style.color = pageBody.style.color === "gold" ? "white" : "gold";
+    pageBody.style.color = pageBody.style.color === "gold" ? "black" : "gold";
+    // refColor.style.color = refColor.style.color === "gold" ? "black" : "gold";
+}
