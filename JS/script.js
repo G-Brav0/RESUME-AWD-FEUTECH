cc2012a7fb014c744534e2c549345afeb52d05f0
G-Brav0 <138981966+G-Brav0@users.noreply.github.com>
2024-02-27 14:39:01 +0800

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -7,8 +7,12 @@ function changeColors() {
     // pageBody.style.backgroundColor = pageBody.style.backgroundColor === "grey" ? "#019938" : "grey";
     // pageBody.style.backgroundColor = pageBody.style.backgroundColor === "teal" ? "grey" : "teal";
     // pageBody.style.backgroundColor = pageBody.style.backgroundColor === "teal" ? "pastel" : "teal";
-    pageBody.style.backgroundColor = pageBody.style.backgroundColor === "teal" ? "turquoise" : "teal";
     
+    // Option 1 (color bg)
+    // pageBody.style.backgroundColor = pageBody.style.backgroundColor === "teal" ? "turquoise" : "teal";
+    // Option 2 (color bg)
+    pageBody.style.backgroundColor = pageBody.style.backgroundColor === "teal" ? "beige" : "teal";
+
     // pageBody.style.color = pageBody.style.color === "gold" ? "white" : "gold";
     pageBody.style.color = pageBody.style.color === "gold" ? "black" : "gold";
     // refColor.style.color = refColor.style.color === "gold" ? "black" : "gold";
